4ec799261cf7c2e5e6474fd2ff33ac5acbd58e6a
Lucas Cardozo Parmegiani <lucasparmegiani@hotmail.com>
2022-02-21 21:09:34 -0300

    Girar Serra
    script girar serra e alguns sons

---
+++ JogoTCC_Unity/Assets/Scripts/Scripts TCC/GiraSerra.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GiraSerra : MonoBehaviour
+{
+
+    [SerializeField] private GameObject serra;
+    public float velocidade = 10;
+
+    void Update()
+    {
+        serra.transform.Rotate(0f, 0f, velocidade * Time.deltaTime);
+    }
+}
